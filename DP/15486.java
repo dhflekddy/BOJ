15275207b652637f54f233691085957d45ab2e8f
hyojin <dhflekddy@hanmail.net>
2023-11-12 23:38:48 +0900

    퇴사2

--- DP/15486.java
+++ DP/15486.java
@@ -5,6 +5,14 @@ import java.util.StringTokenizer;
 /*
 어려운 문제다... dp의 점화식을 구하기 위해서는 무조건 규칙을 찾아 보기 위해 해보는 방법 밖에는 없다.
 https://www.youtube.com/watch?v=-4wjSUr1_K0&t=1458s
+
+비슷비슷한 문제일지라도 문제의 조건과 무엇을 구하는지에 따라 설계기법이 달라진다. 
+마감시간과 작업일수가 있는 스캐줄링 에서 최소시간외 근무일수(27112, 시간외 근무 멈춰). 단 안되면 그냥 -1출력 =>Greedy
+
+마감시간이 있는 스케줄링에서 최대이익(작업일수 없음, 마감시간과 이득만 있음) => Greedy 
+
+(퇴사2문제)작업일수가 있는 스케줄링에서 최대이익 (작업일수, 이득만 있음) =Dynamic
+
  */
 public class Main{
     public static void solution(int n, int[]duration, int[]profit){
