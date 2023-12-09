c1051a42c1d753c40b906949e1740c16bef9a4fd
hyojin <dhflekddy@hanmail.net>
2023-12-09 21:57:43 +0900

    단지번호붙이기

--- GRAPH/2667.java
+++ GRAPH/2667.java
@@ -1,58 +1,76 @@
-//   구현 포인트!
-//   1.  visited 방문배열 생성
-//   2.  Collection class에 요소로 배열을 넣고 그 각각의 배열에 y,x좌표를 넣는 방식이 익숙치 않았음.
-//   3.  방문한좌표는 두번다시 방문하지 못하므로 어떤 점에 처음방문하게 되면 그것이 최단거리가 되고 그 점은 방문처리가 되어 불필요한 좌표값의 증가를 막는다
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    public static void bfs(int[][] map, int y, int x) {
-        int[] dy = {-1, 1, 0, 0};
-        int[] dx = {0, 0, -1, 1};
-        int height = map.length;
-        int width = map[0].length;
-        boolean[][] visited = new boolean[height][width];
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{y, x});
+public class Testing3 {
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int size;
+
+    static class Point {
+        int y;
+        int x;
+
+        Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    public static boolean isValid(int[][] graph, boolean[][] visited, int i, int j) {
+        return (i >= 0 && i < size && j >= 0 && j < size && !visited[i][j] && graph[i][j] == 1);
+    }
+
+    public static int bfs(int[][] graph, boolean[][] visited, Point p, List<Integer> answer) {
+        int result = 0;
+        Queue<Point> que = new LinkedList<>();
+        que.add(p);
+        ++result;
+        visited[p.y][p.x]=true;
         while (!que.isEmpty()) {
-            int[] cur = que.poll();
-            int curY = cur[0];
-            int curX = cur[1];
-            for (int i = 0; i < 4; i++) {
-                int nextY = curY + dy[i];
-                int nextX = curX + dx[i];
-                if (nextY < 0 || nextY >= height || nextX < 0 || nextX >= width)
-                    continue;
-                if (map[nextY][nextX] == 0 || visited[nextY][nextX])//방문한좌표는 두번다시 방문하지 못하므로
-                    //어떤 점에 처음방문하게 되면 그것이 최단거리가 되고 그 점은 방문처리가 되어 불필요한 좌표값의 증가를 막는다.
-                    continue;
-                visited[nextY][nextX] = true;
-                map[nextY][nextX] = map[curY][curX] + 1;
-                que.add(new int[]{nextY,nextX});
+            Point cur = que.poll();
+            for (int i = 0; i < 4; ++i) {
+                int ny = cur.y + dy[i];
+                int nx = cur.x + dx[i];
+                if (isValid(graph, visited, ny, nx)) {
+                    que.add(new Point(ny, nx));
+//                    System.out.println("더해지는 좌표:"+ny+", "+nx);
+                    ++result;
+                    visited[ny][nx] = true;
+                }
             }
         }
+        return result;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void solution(int[][] graph) {
+        List<Integer> answer = new ArrayList<>();
+        size = graph.length;
+        boolean[][] visited = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!visited[i][j] && graph[i][j] == 1) {
+                    Point p = new Point(i, j);
+                    answer.add(bfs(graph, visited, p, answer));
+                }
+            }
+        }
+        Collections.sort(answer);
+        System.out.println(answer.size());
+        answer.stream().forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] map = new int[n][m];
+        int n = Integer.parseInt(br.readLine());
+        int[][] graph = new int[n][n];
+        char[] arr;
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = str.charAt(j) - '0';
+            arr = br.readLine().toCharArray();
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = arr[j] - '0';
             }
         }
-        bfs(map, 0, 0);
-        System.out.println(map[n - 1][m - 1]);
+        solution(graph);
     }
-
 }
